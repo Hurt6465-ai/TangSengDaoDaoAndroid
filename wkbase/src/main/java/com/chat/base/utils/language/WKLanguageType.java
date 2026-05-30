28758dd92438d9ec3b59e0cd07e01842c753b7aa
Hurt6465-ai <hurt6465@gmail.com>
2026-05-31 05:43:30 +0630

    Update WKLanguageType.java

--- wkbase/src/main/java/com/chat/base/utils/language/WKLanguageType.java
+++ wkbase/src/main/java/com/chat/base/utils/language/WKLanguageType.java
@@ -3,14 +3,14 @@ package com.chat.base.utils.language;
 import com.chat.base.WKBaseApplication;
 
 /**
- * Created by lx on 17-11-6.
+ * 多语言类型
  */
-
 public class WKLanguageType {
-    public static final int LANGUAGE_FOLLOW_SYSTEM = 0; //跟随系统
-    public static final int LANGUAGE_EN = 1;    //英文
-    public static final int LANGUAGE_CHINESE_SIMPLIFIED = 2; //简体
-    public static final int LANGUAGE_CHINESE_TRADITIONAL = 3;  //香港台湾繁体
+    public static final int LANGUAGE_FOLLOW_SYSTEM = 0; // 跟随系统
+    public static final int LANGUAGE_EN = 1; // 英文
+    public static final int LANGUAGE_CHINESE_SIMPLIFIED = 2; // 简体
+    public static final int LANGUAGE_CHINESE_TRADITIONAL = 3; // 繁体
+    public static final int LANGUAGE_MYANMAR = 4; // 缅甸语
 
     public static boolean isCN() {
         return WKBaseApplication.getInstance().getContext().getResources().getConfiguration().locale.getCountry().equals("CN");
