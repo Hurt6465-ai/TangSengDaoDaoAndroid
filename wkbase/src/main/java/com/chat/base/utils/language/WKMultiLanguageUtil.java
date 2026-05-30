ebadab715189725aba14220782224791f1e2fafe
Hurt6465-ai <hurt6465@gmail.com>
2026-05-31 05:43:04 +0630

    Update WKMultiLanguageUtil.java

--- wkbase/src/main/java/com/chat/base/utils/language/WKMultiLanguageUtil.java
+++ wkbase/src/main/java/com/chat/base/utils/language/WKMultiLanguageUtil.java
@@ -22,11 +22,10 @@ import java.util.Locale;
  * 多语言切换的帮助类
  */
 public class WKMultiLanguageUtil {
-
     private static final String TAG = "MultiLanguageUtil";
-
     private WeakReference<Context> mContext;
     private static final String SAVE_LANGUAGE = "save_language";
+    private static final int LANGUAGE_MYANMAR = 4;
 
     private static class MultiLanguageUtilBinder {
         private final static WKMultiLanguageUtil util = new WKMultiLanguageUtil();
@@ -47,7 +46,7 @@ public class WKMultiLanguageUtil {
      * 设置语言
      */
     public void setConfiguration() {
-        if (mContext != null) {
+        if (mContext != null && mContext.get() != null) {
             Locale targetLocale = getLanguageLocale();
             Configuration configuration = mContext.get().getResources().getConfiguration();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
@@ -57,12 +56,10 @@ public class WKMultiLanguageUtil {
             }
             Resources resources = mContext.get().getResources();
             DisplayMetrics dm = resources.getDisplayMetrics();
-            resources.updateConfiguration(configuration, dm);//语言更换生效的代码!
+            resources.updateConfiguration(configuration, dm);
         }
-
     }
 
-    //如果不是英文、简体中文、繁体中文，默认返回简体中文
     private Locale getLanguageLocale() {
         if (mContext == null || mContext.get() == null) return getSysLocale();
         int languageType = WKSharedPreferencesUtil.getInstance().getInt(WKMultiLanguageUtil.SAVE_LANGUAGE, 0);
@@ -74,6 +71,8 @@ public class WKMultiLanguageUtil {
             return Locale.SIMPLIFIED_CHINESE;
         } else if (languageType == WKLanguageType.LANGUAGE_CHINESE_TRADITIONAL) {
             return Locale.TRADITIONAL_CHINESE;
+        } else if (languageType == LANGUAGE_MYANMAR) {
+            return new Locale("my", "MM");
         }
         getSystemLanguage(getSysLocale());
         Log.e(TAG, "getLanguageLocale" + languageType + languageType);
@@ -84,28 +83,21 @@ public class WKMultiLanguageUtil {
         return locale.getLanguage() + "_" + locale.getCountry();
     }
 
-    //以上获取方式需要特殊处理一下
     public Locale getSysLocale() {
         Locale locale;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             try {
-                LocaleListCompat listCompat= ConfigurationCompat.getLocales(Resources.getSystem().getConfiguration());
-                locale= listCompat.get(0);
-            }catch (Exception e){
-                locale=LocaleList.getDefault().get(0);
+                LocaleListCompat listCompat = ConfigurationCompat.getLocales(Resources.getSystem().getConfiguration());
+                locale = listCompat.get(0);
+            } catch (Exception e) {
+                locale = LocaleList.getDefault().get(0);
             }
         } else {
             locale = Locale.getDefault();
-
         }
         return locale;
     }
 
-    /**
-     * 更新语言
-     *
-     * @param languageType
-     */
     public void updateLanguage(int languageType) {
         WKSharedPreferencesUtil.getInstance().putInt(WKMultiLanguageUtil.SAVE_LANGUAGE, languageType);
         WKMultiLanguageUtil.getInstance().setConfiguration();
@@ -119,26 +111,27 @@ public class WKMultiLanguageUtil {
             return mContext.get().getString(R.string.setting_simplified_chinese);
         } else if (languageType == WKLanguageType.LANGUAGE_CHINESE_TRADITIONAL) {
             return mContext.get().getString(R.string.setting_traditional_chinese);
+        } else if (languageType == LANGUAGE_MYANMAR) {
+            return mContext.get().getString(R.string.setting_language_myanmar);
         }
         return mContext.get().getString(R.string.setting_language_auto);
     }
 
-    /**
-     * 获取到用户保存的语言类型
-     *
-     * @return
-     */
     public int getLanguageType() {
         int languageType = WKSharedPreferencesUtil.getInstance().getInt(WKMultiLanguageUtil.SAVE_LANGUAGE, WKLanguageType.LANGUAGE_FOLLOW_SYSTEM);
         if (languageType == WKLanguageType.LANGUAGE_CHINESE_SIMPLIFIED) {
             return WKLanguageType.LANGUAGE_CHINESE_SIMPLIFIED;
         } else if (languageType == WKLanguageType.LANGUAGE_CHINESE_TRADITIONAL) {
             return WKLanguageType.LANGUAGE_CHINESE_TRADITIONAL;
+        } else if (languageType == WKLanguageType.LANGUAGE_EN) {
+            return WKLanguageType.LANGUAGE_EN;
+        } else if (languageType == LANGUAGE_MYANMAR) {
+            return LANGUAGE_MYANMAR;
         } else if (languageType == WKLanguageType.LANGUAGE_FOLLOW_SYSTEM) {
             return WKLanguageType.LANGUAGE_FOLLOW_SYSTEM;
         }
         Log.e(TAG, "getLanguageType" + languageType);
-        return languageType;
+        return WKLanguageType.LANGUAGE_FOLLOW_SYSTEM;
     }
 
     public Context attachBaseContext(Context context) {
