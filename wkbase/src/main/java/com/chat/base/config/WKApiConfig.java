b1dc6dc5474aca782765ff5c64bd710f7f3a5c35
Hurt6465-ai <hurt6465@gmail.com>
2026-06-18 18:01:09 +0630

    Update WKApiConfig.java

--- wkbase/src/main/java/com/chat/base/config/WKApiConfig.java
+++ wkbase/src/main/java/com/chat/base/config/WKApiConfig.java
@@ -4,6 +4,8 @@ import android.text.TextUtils;
 
 import com.xinbida.wukongim.entity.WKChannelType;
 
+import java.net.URLEncoder;
+
 /**
  * 2019-11-20 10:11
  * api地址
@@ -11,6 +13,11 @@ import com.xinbida.wukongim.entity.WKChannelType;
 public class WKApiConfig {
     public static String baseUrl = "";
     public static String baseWebUrl = "";
+
+    public static final String NODEBB_BASE_URL = "https://bbs.886.best";
+    public static final String NODEBB_HOME_URL = NODEBB_BASE_URL + "/";
+    public static final String NODEBB_PARTNERS_SWIPE_URL = NODEBB_BASE_URL + "/partners/swipe";
+    public static final String NODEBB_SSO_API_BASE_URL = "https://api.886.best/v1/";
 
     public static void initBaseURL(String apiURL) {
         baseUrl = apiURL + "/v1/";
@@ -20,6 +27,17 @@ public class WKApiConfig {
     public static void initBaseURLIncludeIP(String apiURL) {
         baseUrl = apiURL + "/v1/";
         baseWebUrl = apiURL + "/web/";
+    }
+
+    public static String getNodeBBSSOUrl(String redirectUrl) {
+        if (TextUtils.isEmpty(redirectUrl)) {
+            redirectUrl = NODEBB_HOME_URL;
+        }
+        try {
+            return NODEBB_SSO_API_BASE_URL + "community/nodebb-login?redirect=" + URLEncoder.encode(redirectUrl, "UTF-8");
+        } catch (Exception e) {
+            return NODEBB_SSO_API_BASE_URL + "community/nodebb-login?redirect=" + redirectUrl;
+        }
     }
 
     public static String getAvatarUrl(String uid) {
