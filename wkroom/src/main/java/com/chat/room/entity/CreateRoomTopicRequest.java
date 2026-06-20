0c4838c740e424b0869848721c77f29d8b9a77b2
Hurt6465-ai <hurt6465@gmail.com>
2026-06-20 19:19:18 +0630

    Create CreateRoomTopicRequest.java

---
+++ wkroom/src/main/java/com/chat/room/entity/CreateRoomTopicRequest.java
@@ -0,0 +1,7 @@
+package com.chat.room.entity;
+
+public class CreateRoomTopicRequest implements java.io.Serializable {
+    public String title;
+    public String tag;
+    public String language;
+}
