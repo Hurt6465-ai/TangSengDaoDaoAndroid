d7da617033a85b31bed0ba759eb66d5a24ddbf67
Hurt6465-ai <hurt6465@gmail.com>
2026-06-27 18:26:58 +0630

    Update TSApplication.kt

--- app/src/main/java/com/test/demo2/TSApplication.kt
+++ app/src/main/java/com/test/demo2/TSApplication.kt
@@ -30,6 +30,7 @@ import com.chat.base.utils.WKTimeUtils
 import com.chat.base.utils.language.WKMultiLanguageUtil
 import com.chat.login.WKLoginApplication
 import com.chat.partner.profile.WKPartnerApplication
+import com.chat.partnerbrowse.WKPartnerBrowseApplication
 import com.chat.push.WKPushApplication
 import com.chat.room.WKRoomApplication
 import com.chat.scan.WKScanApplication
@@ -124,6 +125,7 @@ class TSApplication : MultiDexApplication() {
         WKPushApplication.getInstance().init(getAppPackageName(), this)
         WKRoomApplication.getInstance().init(this)
         WKPartnerApplication.getInstance().init(this)
+        WKPartnerBrowseApplication.getInstance().init(this)
         addAppFrontBack()
         addListener()
     }
