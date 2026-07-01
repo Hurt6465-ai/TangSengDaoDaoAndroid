8007c6b607237b3aa516ad1f14ed0d4d344f0244
Hurt6465-ai <hurt6465@gmail.com>
2026-07-01 11:08:45 +0630

    Delete wkuikit/src/main/java/com/chat/uikit/rtc directory

--- wkuikit/src/main/java/com/chat/uikit/rtc/RtcSignalDelegate.java
+++
@@ -1,7 +0,0 @@
-package com.chat.uikit.rtc;
-
-import com.chat.uikit.rtc.model.RtcSignal;
-
-public interface RtcSignalDelegate {
-    void onRtcSignal(RtcSignal signal);
-}
--- wkuikit/src/main/java/com/chat/uikit/rtc/RtcSignalTransport.java
+++
@@ -1,6 +0,0 @@
-package com.chat.uikit.rtc;
-
-public interface RtcSignalTransport {
-    /** Send payload to peerUid by WuKong IM. Payload already contains RtcConstants.SIGNAL_PREFIX. */
-    void sendSignal(String peerUid, String payload) throws Exception;
-}
